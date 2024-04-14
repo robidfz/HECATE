6aea2bd6101b0dd98b4c5a8e624f358520697b1d
robidfz <114222539+robidfz@users.noreply.github.com>
2024-04-14 19:05:46 +0200

    refiniments

--- 00_BN.py
+++ 00_BN.py
@@ -17,6 +17,22 @@ def ageGrouping(x):
     else:
         group=3
     return group
+def preprocessing(config):
+    reader = ConfigParser()
+    reader.read(config)
+    features_initials = reader['FEATURES']['f_list'].split(',')
+    property = reader['ADDITIVE FEATURES']['property']
+    if (property == 'true'):
+        additive_features = reader['ADDITIVE FEATURES']['f_list'].split(',')
+    target = reader['NETWORK SETTING']['target'].split(',')
+    net_number = int(reader['NETWORK SETTING']['number_of_net'])
+    setting = reader['PREPROCESSING SETTING']['property']
+    if (setting == 'true'):
+        feature = reader['PREPROCESSING SETTING']['value']
+        new_feature = reader['PREPROCESSING SETTING']['new_feature']
+        df[new_feature] = df[feature].apply(ageGrouping)
+
+        return net_number,features_initials,additive_features,target
 def buildingDataset(df,features_initials, sg):
     features=features_initials.copy()
     features.append(sg)
@@ -38,6 +54,11 @@ def drawing_net(model,features_initials,sg):
     plt.savefig('BN_'+sg+'.pdf')
     plt.close()
 
+def CDP_estimation(model,data,target):
+    mle = MaximumLikelihoodEstimator(model=model, data=data)
+    cdps = mle.estimate_cpd(node=target)
+    cdps.to_csv(target + "_CDP.csv")
+
 
 #Tumor type---> Met 0 Primary 1 ExtraHepatic 2
 if __name__ == "__main__":
@@ -45,28 +66,14 @@ if __name__ == "__main__":
         config = sys.argv[1]
         dataset=sys.argv[2]
         df = pd.read_csv(dataset)
-        reader = ConfigParser()
-        reader.read(config)
-        features_initials=reader['FEATURES']['f_list'].split(',')
-        property=reader['ADDITIVE FEATURES']['property']
-        if(property=='true'):
-            additive_features=reader['ADDITIVE FEATURES']['f_list'].split(',')
-        target=reader['NETWORK SETTING']['target'].split(',')
-        net_number=int(reader['NETWORK SETTING']['number_of_net'])
-        setting=reader['PREPROCESSING SETTING']['property']
-        if(setting=='true'):
-            feature=reader['PREPROCESSING SETTING']['value']
-            new_feature=reader['PREPROCESSING SETTING']['new_feature']
-            df[new_feature]=df[feature].apply(ageGrouping)
+        net_number,features_initials, additive_features, target= preprocessing(config)
         for i in range(net_number):
             features_initials.append(additive_features[i])
             couples,data=buildingDataset(df,features_initials,target[i])
             model = BayesianNetwork(couples)
             drawing_net(model,features_initials,target[i])
             model.fit(data, estimator=MaximumLikelihoodEstimator)
-            mle = MaximumLikelihoodEstimator(model=model, data=data)
-            cdps=mle.estimate_cpd(node=target[i])
-            cdps.to_csv(target[i]+"_CDP.csv")
+            CDP_estimation(model,data,target[i])
             features_initials.remove(additive_features[i])
     else:
         print('error')
